9f0dc0040252d51dde58db29647e11b40956dd1f
User <User@NPK107-3>
2023-12-04 14:22:54 +0700

    Изменения12

---
+++ Demik/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Demik
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Попробуйте преобразовать введенные значения в числа и выполнить сложение
+            if (int.TryParse(TextBox1.Text, out int value1) && int.TryParse(TextBox2.Text, out int value2))
+            {
+                // Сложение
+                int result = value1 + value2;
+
+                // Установка результата в третий TextBox
+                ResultTextBox.Text = result.ToString();
+            }
+            else
+            {
+                // Если введенные значения не являются числами, выведите сообщение об ошибке или предпримите необходимые действия
+                ResultTextBox.Text = "Invalid input";
+            }
+        }
+
+        private void ResultTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DemikTests/MainWindowTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demik;
+using System;
+
+namespace Demik.Tests
+{
+    [TestClass()]
+    public class DemikTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            // Arrange
+            int value1 = 1;
+            int value2 = 1;
+            int expected = 2;
+
+            // Act
+            int actual = value1 + value2;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
